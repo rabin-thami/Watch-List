9bba1d70b4abde24c3e52f118fafe4ba9b77b4cb
Rabin Thami <rabinthami36@gmail.com>
2023-12-12 18:52:24 +0545

    little change

--- script/index.js
+++ script/index.js
@@ -1,13 +1,18 @@
 const icons = document.querySelectorAll('ul li i');
 
-icons.forEach((icon) => {
-  icon.addEventListener('mouseenter', () => {
-    icon.classList.add('fa-solid');
-    icon.classList.remove('fa-light');
+function leftMenuSetting() {
+  icons.forEach((icon) => {
+    icon.addEventListener('mouseenter', () => {
+      icon.classList.add('fa-solid');
+      icon.classList.remove('fa-light');
+    });
+  
+    icon.addEventListener('mouseleave', () => {
+      icon.classList.remove('fa-solid');
+      icon.classList.add('fa-light');
+    });
   });
+}
 
-  icon.addEventListener('mouseleave', () => {
-    icon.classList.remove('fa-solid');
-    icon.classList.add('fa-light');
-  });
-});
+
+leftMenuSetting()
