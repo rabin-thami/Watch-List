e4194bcdb75954a03549d094eae20a2c636fb01d
Rabin Thami <rabinthami36@gmail.com>
2023-12-14 22:29:13 +0545

    js rewrite

--- script/index.js
+++ script/index.js
@@ -1,82 +1,106 @@
 const formEl = document.querySelector("#searchForm")
-let contentWrapEl = document.querySelector(".content-wrapper")
+const contentWrapperEl = document.querySelector(".content-wrapper")
+let currentMovie = "Avengers"
+checkApiStatus(currentMovie)
 
-formEl.addEventListener('submit', async (e) => {
+formEl.addEventListener('submit', async(e) =>{
     e.preventDefault()
-    const userSearch = formEl.querySelector("#searchMovie").value
-    // console.log(userSearch)
-    formEl.querySelector("#searchMovie").value = ""
-
-    await getSearchMovie(userSearch)
+    const userSearchedMovie = formEl.querySelector('#searchMovie').value
+    formEl.querySelector('#searchMovie').value = ""
+    //exchang the default value
+    currentMovie = userSearchedMovie;
+    await checkApiStatus(userSearchedMovie)
 })
 
-async function getSearchMovie(movieName) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=a6f5f021&s=${movieName}`)
-    const dataFromApi =  await response.json()
-    
-    if(dataFromApi.Response === "True") {
-        renderSearchMovie(dataFromApi.Search)``
-    }else {
-        renderError(dataFromApi.Error, movieName)
+// checking api status
+async function checkApiStatus(movieName) {
+    const response = await fetch(`https://www.omdbapi.com/?apikey=a6f5f021&s=${movieName}`);
+
+    if (response.ok) {
+        const dataFromApi = await response.json();
+        checkApiResponse(dataFromApi, movieName);
+    } else {
+        console.log('Error in API');
     }
 }
 
 
-
-
-function renderSearchMovie(movieData) {
-    // console.log(movieData)
-    let movieHTML = ""
-
-    movieData.forEach(movie => {
-        console.log(movie.Title)
-        movieHTML += `
-        <div class="movie-container">
-                        <div class="poster-container">
-                            <img src="${movie.Poster}" alt="">
-                        </div>
-                        <div class="info-container">
-                            <span>
-                                <i class="fa-light fa-bookmark"></i>
-                            </span>
-                            <div class="information">
-                                <h2 class="title" id="${movie.imdbID}">${movie.Title}</h2>
-                                <p class="type">Type : ${movie.Type}</p>
-                                <p class="releases-year">Year: ${movie.Year}</p>
-                            </div>
-                        </div>
-                    </div>
-        `
-    })
-    contentWrapEl.innerHTML = movieHTML
-
+// checking api's response
+function checkApiResponse(data, movieTitle){
+    if(data.Response === "True"){
+        renderMovie(data.Search, data.totalResults)
+    }else {
+        renderError(data.Error, movieTitle)
+    }
 }
 
-
-function renderError(msg, movieName) {
-    contentWrapEl.innerHTML = `
+// rendeing not found error
+function renderError(msg, title) {
+    contentWrapperEl.innerHTML = `
     <div class="error-msg-container">
-        <h1>"${movieName}", ${msg}</h1>
+        <h1>Your "${title}", ${msg}</h1>
     </div>
     `
 }
 
+// render the data 
+function renderMovie(moveList, totalSearch){
+    // console.log(moveList, totalSearch);
 
-
-const icons = document.querySelectorAll("ul li i")
-function leftMenuHover() {
-    icons.forEach((icon) => {
-      icon.addEventListener('mouseenter', () => {
-        icon.classList.add('fa-solid');
-        icon.classList.remove('fa-light');
-      });
-    
-      icon.addEventListener('mouseleave', () => {
-        icon.classList.remove('fa-solid');
-        icon.classList.add('fa-light');
-      });
+    let containerHTML = ""
+    moveList.forEach(movie => {
+        containerHTML += `
+            <div class="movie-container">
+                <div class="poster-container">
+                    <img src="${movie.Poster}" alt="">
+                </div>
+                <div class="info-container">
+                    <span>
+                        <i class="fas fa-bookmark"></i>
+                    </span>
+                    <div class="information">
+                        <h2 class="title" id="${movie.imdbID}">${movie.Title}</h2>
+                        <p class="type">Type : ${movie.Type}</p>
+                        <p class="releases-year">Year: ${movie.Year}</p>
+                    </div>
+                </div>
+        </div>`
     });
-  }
+    contentWrapperEl.innerHTML = containerHTML;
+    pagination(totalSearch)
+}
 
+//
 
-leftMenuHover()
+function pagination(page) {
+    let paginationEl = document.querySelector(".pagination");
+    let currentPage = 1;
+    let totalPage = Math.ceil(page / 10);
+
+    paginationEl.innerHTML = `
+        <button class="" id="previous">
+            <i class="fas fa-angle-left"></i>
+            <span>Previous</span>
+        </button>
+        <span id="currentPage"> ${currentPage} of ${totalPage} </span>
+        <button class="" id="next">
+            <span>Next</span>
+            <i class="fas fa-angle-right"></i>
+        </button>`;
+
+    let paginationBtn = document.querySelectorAll(".pagination button");
+    paginationBtn.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const buttonId = e.currentTarget.id || e.target.parentElement.id;
+
+            if (buttonId === "next" || buttonId === "Next") {
+                currentPage++;
+            } else if (buttonId === "previous" || buttonId === "Previous") {
+                currentPage--;
+            }
+
+            // Update the content of the #currentPage span
+            document.getElementById("currentPage").innerText = `${currentPage} of ${totalPage}`;
+        });
+    });
+}
